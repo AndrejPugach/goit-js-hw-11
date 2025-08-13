df69046b90118902969bb36209f703f27a42b26b
Andrej <andrej090287@gmail.com>
2025-08-13 22:18:33 +0300

    HWjs11.7

--- src/main.js
+++ src/main.js
@@ -1,50 +1,4 @@
-
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import { getImagesByQuery } from './js/pixabay-api.js';
-import { createGallery, clearGallery,showLoader, hideLoader} from './js/render-functions.js';
-
-const form = document.querySelector('form');
-const submitBtn = form.querySelector('button[type="submit"]');
-
- 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-
-        const formData = new FormData(e.target);
-        const message = formData.get('searchText').trim();
-    
-        if (!message) {
-            iziToast.error({
-                position: 'topRight',
-                title: 'Ерор',
-                message: 'Ви нічого не ввели!'
-            });
-            return;
-    }
-        clearGallery();
-        showLoader();
-
-        submitBtn.disabled = true;
-
-        
-        getImagesByQuery(message).then(result => {
-   
-            const images = result.data.hits;
-        
-            if (!images.length) {
-                iziToast.error({
-                    position: 'topRight',
-                    title: 'Немає результатів',
-                    message: 'Нічого не знайдено'
-                });
-                submitBtn.disabled = false;
-               submitBtn.textContent = oldText;
-                return;
-            }
-
-   import { getImagesByQuery } from "./js/pixabay-api";
+import { getImagesByQuery } from "./js/pixabay-api";
 import { clearGallery, createGallery, hideLoader, showLoader } from "./js/render-functions";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
@@ -89,20 +43,4 @@ formElem.addEventListener('submit', async e => {
     }
 
     
-});         createGallery(images)
-                ;
-        }).catch((err) => {console.log(err);
-        
-            iziToast.error({
-                position: 'topRight',
-                title: err,
-                message: 'Помилка'
-            })
-
-        }).then(() => {
-           hideLoader();
-           submitBtn.disabled = false;
-            e.target.reset();
-        });
-
-})
+});
